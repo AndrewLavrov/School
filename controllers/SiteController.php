d86007f798322339f19f773933d60add84518799
AndrewLavrov <andreylavrov74@gmail.com>
2019-09-22 18:28:19 +0300

    move join and login actions to user controller

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -10,20 +10,7 @@ class SiteController extends Controller
 {
     public function actionIndex ()
 {
-    Yii::trace("I am here!", "site");
-    Yii::info("some information", "site");
-    Yii::warning("low memory", "site");
-    Yii::error("Fatalist", "site");
     return $this->render('index');
 }
 
-    public function actionJoin ()
-    {
-        return $this->render('join');
-    }
-
-    public function actionLogin ()
-    {
-        return $this->render('login');
-    }
 }

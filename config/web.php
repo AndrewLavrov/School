5f997a7b86e44b8fb09fa19a8135dcd6df4aac70
AndrewLavrov <andreylavrov74@gmail.com>
2019-09-22 17:56:18 +0300

    add debug line 2

--- config/web.php
+++ config/web.php
@@ -3,6 +3,7 @@
 return [
     'id' => 'school',
     'basePath' => realpath(__DIR__.'/../'),
+    'bootstrap' => ['debug'],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm' => '@vendor/npm-asset',
@@ -11,7 +12,16 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false
+        ],
+        'request' => ['cookieValidationKey' => 'super secret code']
+    ],
+    'modules' => [
+        'debug' => [
+            'class' => 'yii\debug\Module'
+            // uncomment and adjust the following to add your IP if you are not connecting from localhost.
+            //'allowedIPs' => ['127.0.0.1', '::1'],
         ]
+
     ]
 ];
 
